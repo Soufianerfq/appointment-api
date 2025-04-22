6b6dde2d629d6a9cd93adda4a0b69698d0d86206
soufianerfq <soufianerafiq1999@gmail.com>
2025-04-22 11:35:08 +0100

    cors update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["http://localhost:5173", "https://appointment-scheduler-kappa.vercel.app/"];
 
 module.exports = allowedOrigins;
